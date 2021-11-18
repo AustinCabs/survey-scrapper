383ea565d7a0a884aa70000ef5c98eaac051a67e
Aus_Cabs <46185867+AustinCabs@users.noreply.github.com>
2021-11-18 17:13:47 +0800

    somethings

--- views/extractor.js
+++ views/extractor.js
@@ -1,3 +1,8 @@
+// var myModal = new bootstrap.Modal(document.getElementById('modal'), {
+//     keyboard: false
+//   })
+
+
 
 data("Surallah")
 document.getElementById("sum").innerHTML = "Surallah";
@@ -15,24 +20,52 @@ data(mun)
 
 function reload() {
     var mun = document.getElementById("select").value;
+    if (mun != "") {
+        data(mun)
+    }else{
+         alert("Select Municipalty first ..")
+    }
     // console.log(mun)
-    data(mun)
     // console.log("clicked")
 }
 
 async function data(mun) {
+  
+    
     try {
+        // myModal.show(myModal)
+        Swal.showLoading()
         let response = await fetch(`/data/${mun}`); // resolves with response headers
         let d = await response.json(); // read body as json
         // console.log(result)
         let b = d.labels;
-        
+
+        console.log(d.datasets.length)
+        console.log(d.datasets)
+        console.log(d.datasets[0].data.length)
+
+        let arr1 = []
+        let arr2 = []
+
         let hindi = d.datasets[0].data;
         
-        let oo =d.datasets[1].data;
+        if (d.datasets.length == 2) {
+             hindi.forEach(element => {
+                   arr1.push(0)
+             });    
+        }else if(d.datasets.length == 1){
+            hindi.forEach(element => {
+                arr1.push(0)
+                arr2.push(0)
+          });
+        }
         
-        let u = d.datasets[2].data;
         
+        let oo = arr1.length != null ? d.datasets[1].data : arr1;
+        
+        let u = arr2.length != null ? d.datasets[2].data : arr2;
+        console.log(oo)
+        console.log(u)
         // let r = list.length - b.length;
         // // document.getElementById("remaining").textContent =`remaining ${r}`
         // // document.getElementById("done").textContent= `done ${b.length}`
@@ -60,11 +93,19 @@ async function data(mun) {
         //  console.log(tr)
          document.getElementById('tbody').innerHTML = tr;
          document.getElementById("done").textContent= `${remaining} remaining`
-    } catch (error) {
+         swal.close();
+
+
+    } catch (error) { 
+        // myModal.show()
+        console.log(error)
+        Swal.showLoading()
         let tr =`<tr><td>Information not available</td</tr>`
         document.getElementById('tbody').innerHTML = tr;
         document.getElementById("total").textContent= `Information not available`
         document.getElementById("done").textContent= `Information not available`
+        swal.close();
+        // myModal.hide()
     }
   
 }
